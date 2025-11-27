b3c702cc729ed705a164c2dd74ca7ab7e67e46c6
sa-es-ir <s.esmaeelinejad@gmail.com>
2025-11-27 18:44:04 +0000

    before meeting

--- MessagingApp/Program.cs
+++ MessagingApp/Program.cs
@@ -1,6 +1,7 @@
 using Azure.AI.OpenAI;
 using MessagingApp.Components;
 using MessagingApp.Models;
+using MessagingApp.Services;
 using System.ClientModel;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -13,34 +14,9 @@ var aiEndPoint = builder.Configuration["AzureOpenAI:EndPoint"];
 var aiApiKey = builder.Configuration["AzureOpenAI:ApiKey"];
 
 builder.Services.AddSingleton(_ => new AzureOpenAIClient(new Uri(aiEndPoint!), new ApiKeyCredential(aiApiKey!)));
-builder.Services.AddSingleton<MessagingApp.Services.ConversationService>();
+builder.Services.AddSingleton<ConversationService>();
 builder.Services.Configure<AssistantOptions>(builder.Configuration.GetSection("AzureOpenAI"));
 
-//string endpoint = builder.Configuration["AZURE_OPENAI_ENDPOINT"];
-//string deployment = builder.Configuration["AZURE_OPENAI_GPT_NAME"];
-
-//IChatClient chatClient =
-//    new AzureOpenAIClient(new Uri(endpoint), new DefaultAzureCredential())
-//    .GetChatClient(deployment)
-//    .AsIChatClient();
-
-//List<ChatMessage> chatHistory = [];
-//while (true)
-//{
-//    Console.WriteLine("Your prompt:");
-//    string? userPrompt = Console.ReadLine();
-//    chatHistory.Add(new ChatMessage(ChatRole.User, userPrompt));
-
-//    Console.WriteLine("AI Response:");
-//    string response = "";
-//    await foreach (ChatResponseUpdate item in
-//        chatClient.GetStreamingResponseAsync(chatHistory))
-//    {
-//        Console.Write(item.Text);
-//        response += item.Text;
-//    }
-//    chatHistory.Add(new ChatMessage(ChatRole.Assistant, response));
-//}
 
 var app = builder.Build();
 
